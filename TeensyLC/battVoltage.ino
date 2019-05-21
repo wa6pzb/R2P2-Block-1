3471facbd61a782c6cef7d1f4fa647d53e7ffa46
Dano <wa6pzb@arrl.net>
2019-05-20 21:13:50 -0700

    Actual battery voltage

--- TeensyLC/battVoltage.ino
+++ TeensyLC/battVoltage.ino
@@ -1,9 +1,13 @@
 char *battVoltage() {
+  int vread;
+  float volt;
   int battv;
   char battvc[12];
   analogReadAveraging(32);
-  battv = analogRead(1);
-  Serial.println(battv);
+  vread = analogRead(1);
+  Serial.println(vread);
+  volt = (3.3 * vread)/1024; // 3.3 is the internal reference
+  battv = volt * 200;        // no decimal points
   //convert to char array
   String volt_str = String(battv, DEC);
   volt_str.toCharArray(battvc, 4);
