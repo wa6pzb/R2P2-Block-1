76e0a71c88efef0e2e266782ef30ebf4cd4dd83d
Dano <wa6pzb@arrl.net>
2019-08-01 19:10:51 -0700

    Block 1 Build 022

--- TeensyLC/battVoltage.ino
+++ TeensyLC/battVoltage.ino
@@ -5,12 +5,13 @@ char *battVoltage() {
   char battvc[12];
   analogReadAveraging(32);
   vread = analogRead(1);
-  Serial.println(vread);
+  //Serial.println(vread);  // raw reading
   volt = (3.3 * vread)/1024; // 3.3 is the internal reference
   battv = volt * 200;        // no decimal points
   //convert to char array
   String volt_str = String(battv, DEC);
   volt_str.toCharArray(battvc, 4);
+  Serial.print( "BATT=");
   Serial.println(battvc);
   return battvc;
 }
